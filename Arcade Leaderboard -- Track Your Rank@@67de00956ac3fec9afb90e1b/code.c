e309536cf6fb583b68c6d4c2692f3cc03845ac2a
MehakBonsre <bonsremehak@gmail.com>
2025-04-04 21:59:00 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

--- Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -4,28 +4,20 @@ void trackPlayerRanks(int ranked[], int n, int player[], int m, int result[]);
 
 int main() {
     int i, n, m;
-
-    // Read the number of ranked scores
     scanf("%d", &n);
     int ranked[n];
     for (i = 0; i < n; i++) {
         scanf("%d", &ranked[i]);
     }
-
-    // Read the number of player scores
     scanf("%d", &m);
     int player[m];
     for (i = 0; i < m; i++) {
         scanf("%d", &player[i]);
     }
-
-    // Array to store the results
     int result[m];
 
-    // Compute player ranks
     trackPlayerRanks(ranked, n, player, m, result);
 
-    // Output the results
     for (i = 0; i < m; i++) {
         printf("%d\n", result[i]);
     }
@@ -37,7 +29,7 @@ void trackPlayerRanks(int ranked[], int n, int player[], int m, int result[]) {
     int rank = 1;
     int ranks[n];
 
-    // Generate ranks for the ranked array (consider duplicate scores)
+
     ranks[0] = rank;
     for (int i = 1; i < n; i++) {
         if (ranked[i] != ranked[i - 1]) {
@@ -46,7 +38,7 @@ void trackPlayerRanks(int ranked[], int n, int player[], int m, int result[]) {
         ranks[i] = rank;
     }
 
-    // Determine each player's rank
+
     int j = n - 1;
     for (int i = 0; i < m; i++) {
         while (j >= 0 && player[i] >= ranked[j]) {
